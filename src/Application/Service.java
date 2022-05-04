f2a90d066b6714d02809937e03b49b5f8f101368
Roxana Enasoaie <roxanaenasoaie44@gmail.com>
2022-05-04 17:47:09 +0300

    4 actions

--- src/Application/Service.java
+++ src/Application/Service.java
@@ -7,6 +7,7 @@ public class Service {
     private List<Client> clients = new ArrayList<Client>();
     private List<Courier> couriers = new ArrayList<Courier>();
     private List<Restaurant> restaurants = new ArrayList<Restaurant>();
+    private List<Order> orders = new ArrayList<Order>();
 
     private final ReadServices readServices = new ReadServices();
 
@@ -37,9 +38,60 @@ public class Service {
         }
     }
 
+    public void addOrder(Order order){
+        orders.add(order);
+    }
+    public void showOrders(){
+        for(var order : orders){
+            System.out.println(order.toString());
+        }
+    }
+
 //    1. Compute the salary of a courier
     public Float computeSalary(Courier courier){
         return courier.getWorkedHours()*courier.getEurPerHour();
     }
 
+//    2. Determine how much money does an order cost
+    public Double computePrice(Order order){
+        Double price = 0.0;
+
+        for(Map.Entry<Dish, Integer> dish : order.getOrderedDishes().entrySet()){
+                price += dish.getKey().getDishPrice() * dish.getValue();
+        }
+
+        return price;
+    }
+
+//    3. Compute the credit for a client (how much money he spent on orders).
+    public Double computeCredit(Client client){
+        Double credit = 0.0;
+
+        for(var order : orders){
+            if(order.getClient().getId().equals(client.getId())){
+                credit += computePrice(order);
+            }
+        }
+
+        return credit;
+    }
+
+//    4. Find out how many orders were from restaurants with a given specific(eg. turkish)
+    public Integer noOfOrders(String specific){
+        Integer numberOfOrders = 0;
+
+        for(var restaurant : restaurants){
+            if(restaurant.getTypeOfCuisine().equals(specific)){
+                for(var order : orders){
+                    if(order.getRestaurant().getId().equals(restaurant.getId())){
+                        numberOfOrders += 1;
+                    }
+                }
+            }
+        }
+
+        return numberOfOrders;
+    }
+
+//    5.
 }
